e7c00261ba426d62b81ace799537af48e3501a04
parkcrystal <9jeworld@naver.com>
2020-05-24 16:04:07 +0900

    돌아올때 반영되는 닉네임

--- app/src/main/java/com/phis/androidbasic01_intent_20200524/MainActivity.kt
+++ app/src/main/java/com/phis/androidbasic01_intent_20200524/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.phis.androidbasic01_intent_20200524
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -61,5 +63,18 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 1000) {
+            if(resultCode == Activity.RESULT_OK) {
+                val newNickname = data?.getStringExtra("nick")
+                editNick.text = newNickname
+            }
+
+         }
     }
 }
